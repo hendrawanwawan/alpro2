f90a543e734e39fc9a0ee0c3596a95caa6246cc0
hendrawanwawan <hendrawanhas6k@gmail.com>
2024-03-27 09:31:17 +0800

    menambahkan sorting-algoritma dan delete prepocessor

--- include-type.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-#include "includes/calculator.cpp"
-int main() {
-    cout <<add(a,b)<<endl;
-    return 0;
-}
---
+++ sorting-algorithms/merge-sort.cpp
@@ -0,0 +1,29 @@
+//
+// Created by ACER on 14/03/2024.
+//
+
+#include <iostream>
+using namespace std;
+
+
+
+void merge(int ar[], int first, int mid, int last) {
+    int i,j,k;
+    i = first;
+    j = mid + 1;
+    k = last;
+    while ( i <= mid  && j <= k) {
+        if( )
+    }
+}
+
+void ms(int ar[], int first, int last) {
+    if
+}
+
+
+int main() {
+    cout <<"Hello world";
+
+    return 0;
+}
