2bfbea81e3fc71b63a36dc36c242c8c02117bb17
hendrawanwawan <hendrawanhas6k@gmail.com>
2024-04-03 14:08:58 +0800

    menambahkan pragma once

---
+++ prepocessor-directives/include2.h
@@ -0,0 +1,5 @@
+//
+// Created by ACER on 03/04/2024.
+//
+
+#include "hello.h"
---
+++ prepocessor-directives/pragma.cpp
@@ -0,0 +1,15 @@
+//
+// Created by ACER on 01/04/2024.
+//
+#include <iostream>
+#include "hello.h"
+#include "include1.h"
+#include "include2.h"
+using namespace std;
+int main() {
+    int a,b;
+    cout <<"masukan nilai : \n";
+    cin >> a;
+    cin >> b;
+    cout << sum(a,b);
+}
